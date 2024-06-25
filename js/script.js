b3310241538e1b56304e746fc0c24fb5103786af
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-06-25 16:12:50 +0200

    [UPD] Created 3 variables in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert('ciao');
+// CHIEDO ALLUTENTE DI INSERIRE IL NUMERO DI KM CHE VUOLE PERCORRERE E LA SUA ETà
+let km = prompt('Quanti KM vuoi percorrere?');
+let eta = prompt('Quanti anni hai?');
+
+// CALCOLO DEL PREZZO AL KM 
+let prezzo_km = (km * 0.21)
+console.log(prezzo_km)
